2f2734108a979b9555b64bf9d8a4a6d3ee7b88bc
katieroseschulz <katiekaterose13@gmail.com>
2018-08-03 15:10:26 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,43 @@
-require 'bundler'
-Bundler.require
-
 class MyApp < Sinatra::Base
-
+ 
+ 
+ 
   get '/' do
-    erb :index
+    "Welcome to the the balancED learning style quiz!"
+  end
+  
+  get '/test' do
+  	"this is a test"
+  end
+  
+  get '/test1' do
+  	erb:index
+  end
+  
+    
+  post '/quizresults' do
+    answers = params.values
+    @value= 0 #at the beginning of the quiz
+   answers.each do |answer| #goes to the array of values
+     @value += answer.to_i #adds up numbers that user chose -- also` convert to integer bc we're taking in numbers
+   end
+    
+    if @value <= 15
+    #"You are a visual learner!"
+      erb:visual
+    
+    elsif @value <= 20
+    #"You are an auditory learner!"
+      erb:auditory
+      
+    elsif @value <=30
+    #"You are a kinesthetic learner!"
+      erb:kinesthetic
+    end
   end
 
+  get '/quiz'do
+  	erb:QUIZ
+  end
+ 
 end
